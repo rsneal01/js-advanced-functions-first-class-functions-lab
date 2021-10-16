c9d4ec4373b3544c54408d76f23c544b9ecd62b0
rsneal01 <rsneal01@gmail.com>
2021-10-16 13:58:13 -0700

    commit tree fed49d653c367cfd7f31b240cd5fed7e722d85c2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(ray){
+    const newRay = ray.slice(0, 2);
+    // debugger;
+    console.log(newRay)
+    return newRay
+}
+
+const returnLastTwoDrivers = function(ray){
+    const newRay = ray.slice(2);
+    return newRay
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier){
+    return function(fare){
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(arrayOfDrivers, driversFunction){
+    return driversFunction(arrayOfDrivers);
+}
